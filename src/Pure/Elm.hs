9b0a5e7f5b7d56bbb5499edad08dd7c32f143bbc
Sean Mackenzie <seanmhickman@gmail.com>
2019-06-13 19:20:00 -0500

    Refine implementation.f

--- src/Pure/Elm.hs
+++ src/Pure/Elm.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE ImplicitParams, ConstraintKinds, RankNTypes, RecordWildCards #-}
-module Pure.Elm (Elm,command,run,module Export) where
+module Pure.Elm (Elm,command,App(..),run,Routed(..),routed,module Export) where
 
-import Pure as Export hiding (initial,update,view)
+import Pure as Export hiding (Home,update,view)
 import qualified Pure (view)
 import Pure.Router as Export hiding (router)
 
@@ -15,38 +15,51 @@ type Elm msg = (?command :: msg -> IO ())
 command :: Elm msg => msg -> IO ()
 command msg = ?command msg
 
-data App rt st msg = App
-  { home    :: rt
-  , initial :: Elm msg => st
-  , router  :: Elm msg => Routing rt rt
-  , update  :: Elm msg => rt -> st -> msg -> IO st
-  , view    :: Elm msg => rt -> st -> View
-  }
+data App st msg = App st (st -> msg -> IO st) (Elm msg => st -> View)
 
-instance (Typeable rt, Typeable st, Typeable msg) => Pure (App rt st msg) where
-  view App {..} =
-    flip Component () $ \self ->
-      let 
-        go r = modify_ self $ \_ (_,st) -> (r,st)
-      in let
-        ?command = fix $ \f -> \msg -> modifyM_ self $ \_ (rt,st) -> do
-                       st' <- let ?command = f in update rt st msg
-                       pure ((rt,st'),pure ())
-      in
-        def { construct = return (home,initial)
-            , mounted   = void $ onRoute' go
-            , render    = \_ (rt,st) ->
-              Div <||>
-                [ view rt st
-                , View (Router home (route router))
-                ]
-            }
+run :: (Typeable st, Typeable msg) => App st msg -> View
+run (App model update view) = 
+  flip Component () $ \self ->
+    let
+      ?command = fix $ \f -> \msg -> modifyM_ self $ \_ st -> do
+                     st' <- let ?command = f in update st msg
+                     pure (st',pure ())  
+    in      
+      def { construct = return model
+          , render    = \_ -> view
+          }
 
-run :: (Typeable rt, Typeable st, Typeable msg) 
-    => rt
-    -> (Elm msg => st)
-    -> (Elm msg => Routing rt rt)
-    -> (Elm msg => rt -> st -> msg -> IO st)
-    -> (Elm msg => rt -> st -> View)
-    -> IO ()
-run home initial router update view = inject body (View App {..})
+instance (Typeable st, Typeable msg) => Pure (App st msg) where
+  view = run
+
+data Routed st msg = Routed (Routing msg ()) (App st msg)
+
+routed :: (Typeable st, Typeable msg) => Routed st msg -> View
+routed (Routed rtr (App model update view)) = flip Component () $ \self ->
+  let 
+    router = fmap Just . route rtr
+    go mm = modify_ self $ \_ _ -> mm
+  in
+    def { construct = return Nothing 
+        , mounted   = void $ onRoute' go
+        , render    = \_ mmsg -> 
+          Div <||>
+            [ Component (\self ->
+              let
+                ?command = fix $ \f -> \msg -> modifyM_ self $ \_ mdl -> do
+                               mdl' <- let ?command = f in update mdl msg
+                               pure (mdl',pure ())  
+              in      
+                def { construct = return model
+                    , receive   = \mmsg mdl -> 
+                      case mmsg of
+                        Nothing  -> pure mdl
+                        Just msg -> ?command msg >> pure mdl
+                    , render    = \_ -> view
+                    }) mmsg
+            , View (Router Nothing router)
+            ]
+        }
+
+instance (Typeable st, Typeable msg) => Pure (Routed st msg) where
+  view = routed
