f5b9f05f99fb957ee3767b8514309a95edfda5f7
Sean Mackenzie <seanmhickman@gmail.com>
2019-06-23 03:53:06 -0500

    Refine app initialization.

--- src/Pure/Elm.hs
+++ src/Pure/Elm.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE ImplicitParams, ConstraintKinds, RankNTypes, RecordWildCards #-}
+{-# LANGUAGE ImplicitParams, ConstraintKinds, RankNTypes, RecordWildCards,
+   ScopedTypeVariables #-}
 module Pure.Elm (Elm,App(..),run,command,map,module Export) where
 
 import Pure as Export hiding (Home,update,view)
@@ -14,26 +15,27 @@ import Prelude hiding (map)
 -- | An implicit constraint to propagate an elm context.
 type Elm msg = (?command :: msg -> IO ())
 
-data App st msg = App 
-  { _model  :: Elm msg => st
-  , _update :: Elm msg => msg -> st -> IO st 
-  , _view   :: Elm msg => st -> View
+data App env st msg = App 
+  { _receive :: env -> msg
+  , _model   :: Elm msg => st
+  , _update  :: Elm msg => msg -> env -> st -> IO st 
+  , _view    :: Elm msg => env -> st -> View
   }
 
 -- | Turn an `App st msg` into a component with `msg` property.
-run :: (Typeable st, Typeable msg) => App st msg -> msg -> View
-run app msg =
-  flip Component (app,msg) $ \self ->
+run :: forall env st msg. (Typeable env, Typeable st, Typeable msg) => App env st msg -> env -> View
+run App {..} env =
+  -- the Proxy tags this Component with the App type to
+  -- improve correctness of diffing.
+  flip Component (Proxy :: Proxy (st,env,msg),env) $ \self ->
     let
-      ?command = fix $ \f -> \msg -> modifyM_ self $ \(app,_) mdl -> do
-                     mdl' <- let ?command = f in _update app msg mdl
+      ?command = fix $ \f -> \msg -> modifyM_ self $ \(_,env) mdl -> do
+                     mdl' <- let ?command = f in _update msg env mdl
                      pure (mdl',pure ())
     in      
-      def { construct = do
-            (app,msg) <- ask self
-            (_update app) msg (_model app)
-          , receive   = \(app,msg) -> _update app msg
-          , render    = \(app,_) -> _view app
+      def { construct = pure _model 
+          , receive   = \(_,env) -> _update (_receive env) env
+          , render    = \(_,env) -> _view env
           }
 
 -- | Given a satisfied `Elm msg` constraint, send a command.
