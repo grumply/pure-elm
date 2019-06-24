94cf81cec31e03fefc472f606c165a426ae5c315
Sean Mackenzie <seanmhickman@gmail.com>
2019-06-24 06:49:22 -0500

    Rewrite subscriptions with STM and support buffering of messages.

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
-{ mkDerivation, base, containers, pure, stdenv }:
+{ mkDerivation, base, containers, stm, pure, stdenv }:
 mkDerivation {
   pname = "pure-elm";
   version = "0.7.0.0";
   src = ./.;
-  libraryHaskellDepends = [ base pure containers ];
+  libraryHaskellDepends = [ base pure containers stm ];
   homepage = "github.com/grumply/pure-elm";
   description = "An implementation of the Elm architecture in Pure";
   license = stdenv.lib.licenses.bsd3;
--- src/Pure/Elm/Subscriptions.hs
+++ src/Pure/Elm/Subscriptions.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE AllowAmbiguousTypes, RecursiveDo, ScopedTypeVariables,
-   ImplicitParams, ConstraintKinds #-}
+   ImplicitParams, ConstraintKinds, TypeApplications #-}
 module Pure.Elm.Subscriptions where
 
-import Control.Concurrent
+import Control.Concurrent.STM
 import Control.Monad
-import Data.IORef
+import Control.Monad.STM
+import Data.Foldable
+import Data.List as List
 import Data.Maybe
 import Data.Traversable
 import Data.Typeable
@@ -13,33 +15,43 @@ import GHC.Exts
 import System.IO.Unsafe
 import Unsafe.Coerce
 
-import Data.Map as Map
+import Data.Map as Map hiding ((\\))
 
 type Elm msg = (?command :: msg -> IO Bool)
 
-type Broker = IORef (Map TypeRep (MVar [(Unique,Any -> IO Bool)]))
+type Dispatcher = Either [Any] [(Unique,Any -> IO Bool)]
+type Broker = TMVar (Map TypeRep Dispatcher)
 
 {-# NOINLINE broker #-}
 broker :: Broker
-broker = unsafePerformIO (newIORef mempty)
+broker = unsafePerformIO (newTMVarIO mempty)
 
-subscribeWith :: forall msg. Typeable msg => (msg -> IO Bool) -> IO Unique
-subscribeWith f = mdo
+-- Be careful not to block in the callback.
+unsafeSubscribeWith :: forall msg. Typeable msg => (msg -> IO Bool) -> IO Unique
+unsafeSubscribeWith f = mdo
   let 
     g = f . unsafeCoerce
     tr = typeOf (undefined :: msg)
   u <- newUnique
-  mv <- join $ atomicModifyIORef' broker $ \b -> 
+  join $ atomically $ do
+    b <- takeTMVar broker
     case Map.lookup tr b of
-      Nothing -> (Map.insert tr mv b,newMVar [(u,g)])
-      Just mv -> (b,modifyMVar_ mv (pure . (++ [(u,g)])) >> pure undefined)
+      Nothing -> do
+        putTMVar broker (Map.insert tr (Right [(u,g)]) b)
+        pure (pure ())
+      Just (Left as) -> do
+        putTMVar broker (Map.insert tr (Right [(u,g)]) b)
+        pure (for_ (Prelude.reverse as) g)
+      Just (Right hs) -> do
+        putTMVar broker (Map.insert tr (Right (hs ++ [(u,g)])) b)
+        pure (pure ())
   pure u
 
 subscribe :: (Typeable msg, Elm msg) => IO ()
-subscribe = void $ subscribeWith ?command
+subscribe = void $ unsafeSubscribeWith ?command
 
 subscribe' :: (Typeable msg, Elm msg) => IO Unique
-subscribe' = subscribeWith ?command
+subscribe' = unsafeSubscribeWith ?command
 
 publish :: Typeable msg => msg -> IO ()
 publish = void . publish'
@@ -49,25 +61,43 @@ publish' msg = do
   let 
     any = unsafeCoerce msg
     tr = typeOf (undefined :: msg)
-  b <- readIORef broker
-  case Map.lookup tr b of
-    Nothing -> pure False
-    Just mv -> do
-      hs <- takeMVar mv
-      hs' <- fmap catMaybes $ for hs $ \(u,h) -> do
-        b <- h any
-        pure $ if b then Just (u,h) else Nothing
-      putMVar mv hs'
-      pure True
+  join $ atomically $ do
+    b <- takeTMVar broker
+    case Map.lookup tr b of
+      Nothing -> do
+        putTMVar broker (Map.insert tr (Left [any]) b)
+        pure (pure False)
+      Just (Left as) -> do
+        putTMVar broker (Map.insert tr (Left (any:as)) b)
+        pure (pure False)
+      Just (Right hs) -> do
+        putTMVar broker b
+        pure $ do
+          hs' <- fmap catMaybes $ for hs $ \(u,h) -> do
+            b <- h any
+            pure $ if b then Just (u,h) else Nothing
+          let dead = fmap fst hs \\ fmap fst hs'
+          case hs' of
+            [] -> do
+              cleanup @msg dead
+              publish' msg
+            _  -> 
+              pure True
+
+cleanup :: forall msg. Typeable msg => [Unique] -> IO ()
+cleanup us = do
+  let tr = typeOf (undefined :: msg)
+  atomically $ do 
+    b <- takeTMVar broker
+    case Map.lookup tr b of
+      Just (Right hs) -> do
+        let hs' = List.filter ((not . (`elem` us)) . fst) hs
+        putTMVar broker (Map.insert tr (Right hs') b)
+      _ -> putTMVar broker b
 
 unsubscribe :: forall msg. (Typeable msg, Elm msg) => Unique -> IO ()
-unsubscribe = unsubscribeWith (Proxy :: Proxy msg)
+unsubscribe u = cleanup @msg [u] 
 
 unsubscribeWith :: forall msg. Typeable msg => Proxy msg -> Unique -> IO ()
-unsubscribeWith _ u = do
-  let tr = typeOf (undefined :: msg)
-  b <- readIORef broker
-  case Map.lookup tr b of
-    Nothing -> pure ()
-    Just mv -> modifyMVar_ mv (pure . Prelude.filter ((/= u) . fst))
+unsubscribeWith _ u = cleanup @msg [u] 
 
