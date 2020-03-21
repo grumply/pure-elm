498d336bd7cf78d3b6be2487baed1baac71225a9
Sean Hickman <sean@grumply.com>
2020-03-21 01:00:55 -0500

    Fix transliteration error in hostname.js

--- js/hostname.js
+++ js/hostname.js
@@ -1,7 +1,7 @@
 var window = window || {};
 
 // modified from: https://stackoverflow.com/questions/8498592/extract-hostname-name-from-string
-window.getHostname = (uri) => {
-  const x = $1.match(/^(?:https?:)?(?:\/\/)?([^\/\?]+)/i);
+window.getHostname = (url) => {
+  const x = url.match(/^(?:https?:)?(?:\/\/)?([^\/\?]+)/i);
   return (x && x[1] || '');
 }
